5348e965d9aeecbde49bfc6ad7fa83b2b3d5de11
Antoni2021 <antonio.bond2013@gmail.com>
2021-07-17 01:05:16 +0300

    Finish the task2

--- Tasks/src/main/java/task2_java/Main.java
+++ Tasks/src/main/java/task2_java/Main.java
@@ -3,6 +3,6 @@ package task2_java;
 public class Main {
 
     public static void main(String[] args) {
-        Mathematics.getAbsolutDifference();        
+        Mathematics.getAbsolutDifference();
     }
 }
